7caac90722104e1c4fb6745041c5832b0e035eaf
Aditya <aditya.pawar23624@gmail.com>
2024-03-02 23:48:01 +0530

    did stuff

---
+++ HackerrankCodes.cpp
@@ -0,0 +1,78 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a,b;
+
+    cout<<""<<endl;
+    cin>>a;
+
+    cout<<""<<endl;
+    cin>>b;
+
+    
+    if(a>0,b>0,a<10,b<10)
+    {
+        if(a,b==1)
+        {
+            cout<<"onen"<<endl;
+        }
+
+    
+        /*else if(n==2)
+        {
+        cout<<"two"<<endl;
+       
+        }
+    
+        else if(n==3){
+        cout<<"three"<<endl;
+        
+        } 
+    
+        else if(n==4){
+        cout<<"four"<<endl;
+        
+        }
+    
+        else if(n==5){
+        cout<<"five"<<endl;
+        
+        }
+    
+        else if(n==6)
+        {
+         cout<<"six"<<endl;
+        
+        }
+    
+        else if(n==7)
+        {
+         cout<<"seven"<<endl;
+        
+        }
+    
+        else if(n==8)
+        {
+         cout<<"eight"<<endl;
+        
+        }
+    
+        else if(n==9){
+        cout<<"nine"<<endl;
+     }*/
+    }
+
+    /*int s=n%2;
+
+    if(s==0)
+    {
+        cout<<"even"<<endl;
+    }
+    else
+    {
+        cout<<"Odd"<<endl;
+    }
+*/
+}
