10169954b364c22f0194a822ee2dbfecddb86ead
Aditya <aditya.pawar23624@gmail.com>
2024-03-03 19:06:48 +0530

    I now have a god complex

--- LoopingHakerrank.cpp
+++ LoopingHakerrank.cpp
@@ -1,92 +1,21 @@
-/*#include<iostream>
+#include <iostream>
+#include <string>
 using namespace std;
 
-int main()
-{
-    int a,b;
-    string arr[9]={"one","two","three","four","five","six","seven","eight","nine"};
-    
-
-    cout<<""<<"";
-    cin>>a>>b;
-
-
-    if(a<10,a>0)
-    {
-        for(int i=a;i<=b;i++)
-        {
-            cout<<arr[i-1]<<endl;
-
-        }
-    }
-
-    int p,q;
-
-    p=a%2;
-    q=b%2;
-
-    if(p==0)
-    {
-        cout<<"even"<<endl;
-    }
-    else
-    {
-        cout<<"odd"<<endl;
-    }
-
-    if(q==0)
-    {
-        cout<<"even"<<endl;
-    }
-    else
-    {
-        cout<<"odd"<<endl;
-    }
-    
-}*/
-
-
-#include<iostream>
-using namespace std;
-
-int main()
-{
-    int a,b;
-    string arr[9]={"one","two","three","four","five","six","seven","eight","nine"};
-    
-
-    cout<<""<<"";
-    cin>>a>>b;
-
-    if(a>0)
-    {
-        for(int i=a;i<=b;i++)
-    {
-        cout<<i<<endl;
-        if(i<10)
-        {
-            cout<<arr[i-1]<<endl;
-        }
-
-
-        else
-        {
-            for(int j=i;j<b;j++)
-            {
-                int p;
-                p=j%2;
-
-                if(p==0)
-                {
-                    cout<<"even"<<endl;
-                }
-                else
-                {
-                    cout<<"odd"<<endl;
-                }
-                
+int main() {
+    int a, b;
+    cin >> a >> b;
+    string arr[] = {"one","two","three","four","five","six","seven","eight","nine"};
+    for(int i = a; i <= b; i++) {
+        if(i < 10) {
+            cout << arr[i - 1] << endl;
+        } else {
+            if(i % 2 == 0) {
+                cout << "even" << endl;
+            } else {
+                cout << "odd" << endl;
             }
         }
     }
-    }
+    return 0;
 }
