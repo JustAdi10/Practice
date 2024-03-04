320bd9bcf9676020d7b3c1852df81de0ecd325a6
Aditya <aditya.pawar23624@gmail.com>
2024-03-04 15:52:24 +0530

    max of 4

--- FunctionsHackerrank.cpp
+++ FunctionsHackerrank.cpp
@@ -1,12 +1,18 @@
 #include<iostream>
 #include<algorithm>
 using namespace std;
-
 int main()
 {
     int a,b,c,d;
     cin>>a>>b>>c>>d;
-
-    cout<<std::max('a','b','c','d');   
-
+    int x=max(a,b);
+    int y=max(c,d);
+    int maxi=max(x,y);
+    cout<<maxi<<endl;
+    return 0;
 }
+
+
+
+
+
