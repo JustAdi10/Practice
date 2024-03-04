fe2a2709d7d00d33d307cd697581610517c58ffb
Aditya <aditya.pawar23624@gmail.com>
2024-03-04 21:34:34 +0530

    GOTEM

--- Hackerrank/Pointers.cpp
+++ Hackerrank/Pointers.cpp
@@ -1,15 +1,16 @@
-#include<iostream>
+#include <iostream>
+#include<cstdlib>
 using namespace std;
 
-int main()
-{
-    int a,b;
-    int *a,*b;
-
-    cin>>a>>b;
-
-    a*=&a;
-    b*=&b;
-
+int main() {
+    int a, b;
+    int *ptr_a = &a;
+    int *ptr_b = &b;
     
+    cin >> *ptr_a >> *ptr_b;
+    
+    cout << *ptr_a + *ptr_b <<endl;
+    cout << abs(*ptr_a - *ptr_b) <<endl;
+    
+    return 0;
 }
