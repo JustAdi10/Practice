a8cdd75018bc639ee6940658e07c781c2c61c3f9
Aditya <aditya.pawar23624@gmail.com>
2024-03-05 23:31:47 +0530

    how do i even do this shi

---
+++ Hackerrank/VariableSizedArrays.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    int arr[n];
+
+    cin>>n;
+
+    for(i=0;i<=n;i++)
+    {
+        int k;
+        scanf("%d",&arr[i]);
+    }
+}
