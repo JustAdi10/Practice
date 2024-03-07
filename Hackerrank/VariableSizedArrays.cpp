9e91181bcf810e7af424cd4a993fa4c51f9bef17
Aditya <aditya.pawar23624@gmail.com>
2024-03-07 10:59:52 +0530

    wcwc

--- Hackerrank/VariableSizedArrays.cpp
+++ Hackerrank/VariableSizedArrays.cpp
@@ -14,17 +14,30 @@ using namespace std;
 
 int main()
 {
-    int narray,nquery;
-    cin>>narray>>" ">>nquery;
+    int narr,nque;
+    cin>>narr>>nque;
 
-    for(i=0;i<=narray;i++)
+    int a[narr][];  
+
+    for(int i=0;i<=narr;i++)
     {
-        int x[i];
-        
+        int nelem1;
+        cin>>nelem1;
+        for(int j=0;j<=nelem1;j++)
+        {
+            int x;
+            cin>>x;
+            a[i][j]=x;
+        }
     }
-    
 
+    int p,q,r,s,u,v;
+    cin>>p>>q;
+    cin>>r>>s;
 
+    u=a[p][q];
+    v=a[r][s];
 
+    cout<<u<<endl<<v;
 
 }
