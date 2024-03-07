62c890adf232ac1c8f6564d5dfbf3e158159686d
Aditya <aditya.pawar23624@gmail.com>
2024-03-07 20:45:33 +0530

    Why are we still here

--- Hackerrank/VariableSizedArrays.cpp
+++ Hackerrank/VariableSizedArrays.cpp
@@ -17,9 +17,9 @@ int main()
     int narr,nque;
     cin>>narr>>nque;
 
-    int a[narr][];  
+    int arr[][];
 
-    for(int i=0;i<=narr;i++)
+    for(int i=0;i<narr;i++)
     {
         int nelem1;
         cin>>nelem1;
@@ -27,17 +27,10 @@ int main()
         {
             int x;
             cin>>x;
-            a[i][j]=x;
+            arr[i][j]=x;
         }
     }
 
-    int p,q,r,s,u,v;
-    cin>>p>>q;
-    cin>>r>>s;
-
-    u=a[p][q];
-    v=a[r][s];
-
-    cout<<u<<endl<<v;
+    cout<<arr[0][1];
 
 }
