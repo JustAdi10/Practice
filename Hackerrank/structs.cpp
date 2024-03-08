7b80515fed19438ce1caffdd8e4b3f0a07baa997
Aditya <aditya.pawar23624@gmail.com>
2024-03-08 09:41:49 +0530

    Classes finally

---
+++ Hackerrank/structs.cpp
@@ -0,0 +1,63 @@
+#include<iostream>
+using namespace std;
+
+class stu
+{
+    public:
+    int age;
+    void i_age()
+    {
+        cin>>age;
+    }
+    void o_age()
+    {
+        cout<<age<<" ";
+    }
+
+    string firstname;
+    void i_firstname()
+    {
+        cin>>firstname;
+    }
+    void o_firstname()
+    {
+        cout<<firstname<<" ";
+    }
+
+    string lastname;
+    void i_lastname()
+    {
+        cin>>lastname;
+    }
+    void o_lastname()
+    {
+        cout<<lastname<<" ";
+    }
+
+    int standard;
+    void i_standard()
+    {
+        cin>>standard;
+    }
+    void o_standard()
+    {
+        cout<<standard;
+    }
+
+
+};
+
+
+int main()
+{
+    stu A;
+    A.i_age();
+    A.i_firstname();
+    A.i_lastname();
+    A.i_standard();
+    A.o_age();
+    A.o_firstname();
+    A.o_lastname();
+    A.o_standard();
+
+}
