74309673c427f05c0ee3b833f1f83bc8d2c7e50e
Aditya <aditya.pawar23624@gmail.com>
2024-03-08 13:04:34 +0530

    try later

---
+++ Hackerrank/Classes_and_objects.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+class student
+{
+    public:
+    int scores[5];
+
+    void inp()
+    {
+        for(i=0;i<=5;i++)
+        {
+           cin>>scores[i];
+        }
+    }
+
+    int calculateTotalScore()
+    {
+        
+    }
+
+};
+
+
+int main()
+{
+
+}
