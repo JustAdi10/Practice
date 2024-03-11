fced3935212ecf42f298522ad06d55fcb695853a
Aditya <aditya.pawar23624@gmail.com>
2024-03-11 15:34:17 +0530

    wowowowowo

---
+++ Udemy/Fav number.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter your favourite number between 1 and 100: ";
+    cin>>n;
+
+    cout<<"Your favourite number is : "<<n<<endl;
+
+    return 0;
+}
---
+++ Udemy/section4Challenge.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter your favourite number between 1 and 100: ";
+    cin>>n;
+
+
+    if(n<=100 && n>0)
+    {
+    cout<<endl<<"Amazing!! That's my favourite number too!"<<endl<<"No really!!, "<<n<<" is my favourite number!";
+    }
+
+}
