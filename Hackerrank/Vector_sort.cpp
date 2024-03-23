bc6d093ec6c04223fa6567250316f5b1ba8dfed1
Aditya <aditya.pawar23624@gmail.com>
2024-03-23 15:51:17 +0530

    ocd

--- Hackerrank/Vector_sort.cpp
+++ Hackerrank/Vector_sort.cpp
@@ -1,25 +1,25 @@
-    #include<iostream>
-    #include<vector>
-    #include<algorithm>
+#include<iostream>
+#include<vector>
+#include<algorithm>
 
-    using namespace std;
+using namespace std;
 
-    int main()
+int main()
+{
+    int n;
+    cin>>n;
+
+    vector <int> vect(n);
+
+    for(int i=0;i<n;i++)
     {
-        int n;
-        cin>>n;
-
-        vector <int> vect(n);
-
-        for(int i=0;i<n;i++)
-        {
-            cin>>vect.at(i);
-        }
-
-        sort(vect.begin(), vect.end());
-
-        for(int i=0;i<n;i++)
-        {
-            cout<<vect.at(i)<<" ";
-        }
+        cin>>vect.at(i);
     }
+
+    sort(vect.begin(), vect.end());
+
+    for(int i=0;i<n;i++)
+    {
+        cout<<vect.at(i)<<" ";
+    }
+}
