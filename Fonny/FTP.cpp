0c6d17ba4c8e967ed223749475196099658b9caa
Aditya <aditya.pawar23624@gmail.com>
2024-03-28 23:46:51 +0530

    Needs Work

--- Fonny/FTP.cpp
+++ Fonny/FTP.cpp
@@ -20,10 +20,32 @@ int main()
 
     for(int i=0;i<=query;i++)
     {
-        for(int j=0;j<4)
+        for(int j=0;j<=4;)
         {
-            int x=array1[query][j]
+            int x=array1[query][j];
+            vector <int> xcoords{};
+            xcoords.push_back(x);
             j=j+2;
+            
+            if(j=4)
+            {
+                int xdisplay=xcoords.at(0)+xcoords.at(1);
+                cout<<xdisplay<<endl;
+            }
+        }
+
+        for(int j=1;j<4;)
+        {
+            int y=array1[query][j];
+            vector <int> ycoords{};
+            ycoords.push_back(y);
+            j=j+2;
+            
+            if(j=3)
+            {
+                int ydisplay=ycoords.at(0)+ycoords.at(1);
+                cout<<ydisplay<<endl;
+            }
         }
     }
 }
