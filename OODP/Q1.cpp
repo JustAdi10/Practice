853239d37b08fe9f55a7743504792c5fc886ec1a
Aditya <aditya.pawar23624@gmail.com>
2024-04-05 10:10:05 +0530

    ...

--- OODP/Q1.cpp
+++ OODP/Q1.cpp
@@ -1,76 +1,114 @@
-#include<iostream>
+// #include<iostream>
+// using namespace std;
+
+// class doctor
+// {
+// public:
+//     string docname;
+//     string docdeg;
+
+//     void get_docname()
+//     {
+//         cin >> docname;
+//     }
+
+//     void getedu()
+//     {
+//         cin >> docdeg;
+//     }
+
+//     void dispdname()
+//     {
+//         cout << "Doctor Name: " << docname << endl;
+//     }
+
+//     void dispedu()
+//     {
+//         cout << "Doctorate Degree: " << docdeg << endl;
+//     }
+
+// };
+
+// class patient
+// {
+// public:
+//     string pname;
+//     int bno;
+
+//     void getdata()
+//     {
+//         cin >> pname;
+//     }
+
+//     void get_bno()
+//     {
+//         cin >> bno;
+//     }
+
+//     void dispdata()
+//     {
+//         cout << "Patient Name: " << pname << endl;
+//     }
+
+//     void dispbno()
+//     {
+//         cout << "Bed Number: " << bno << endl;
+//     }
+// };
+
+
+// int main()
+// {
+//     doctor d;
+//     patient p;
+
+//     d.get_docname();
+//     d.getedu();
+//     p.getdata();
+//     p.get_bno();
+
+//     d.dispdname();
+//     d.dispedu();
+//     p.dispdata();
+//     p.dispbno();
+
+//     return 0;
+// }
+
+
+#include <iostream>
 using namespace std;
-
-class doctor
+class consumer
 {
-public:
-    string docname;
-    string docdeg;
-
-    void get_docname()
-    {
-        cin >> docname;
-    }
-
-    void getedu()
-    {
-        cin >> docdeg;
-    }
-
-    void dispdname()
-    {
-        cout << "Doctor Name: " << docname << endl;
-    }
-
-    void dispedu()
-    {
-        cout << "Doctorate Degree: " << docdeg << endl;
-    }
-
+    public:
+    virtual void getdata()=0;
+    virtual void display()=0;
 };
-
-class patient
+class transaction:public consumer
 {
-public:
-    string pname;
-    int bno;
-
-    void getdata()
-    {
-        cin >> pname;
+    public:
+    char name[50];
+    int cd,qty,pri,Tpri;
+    long int Tn;
+    void getdata(){
+    cin>>name;
+    cin>>cd>>Tn>>qty>>pri;
+    Tpri=qty*pri;
     }
-
-    void get_bno()
-    {
-        cin >> bno;
-    }
-
-    void dispdata()
-    {
-        cout << "Patient Name: " << pname << endl;
-    }
-
-    void dispbno()
-    {
-        cout << "Bed Number: " << bno << endl;
+    void display(){
+    cout<<"Name : "<<name<<endl;
+    cout<<"Code : "<<cd<<endl;
+    cout<<"Telephone : "<<Tn<<endl;
+    cout<<"Quantity : "<<qty<<endl;
+    cout<<"Price : "<<pri<<endl;
+    cout<<"Total Price : "<<Tpri<<endl;
     }
 };
-
-
-int main()
-{
-    doctor d;
-    patient p;
-
-    d.get_docname();
-    d.getedu();
-    p.getdata();
-    p.get_bno();
-
-    d.dispdname();
-    d.dispedu();
-    p.dispdata();
-    p.dispbno();
-
-    return 0;
+int main(){
+consumer *o1;
+transaction o2;
+o1=&o2;
+o1->getdata();
+o1->display();
+return 0;
 }
