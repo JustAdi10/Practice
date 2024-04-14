32729445a6299a6c8d7d0655a0d0c95fa3b69d0d
Aditya <aditya.pawar23624@gmail.com>
2024-04-14 23:32:23 +0530

    you are my sunshine

---
+++ CwH_Cpp/Tut4.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+int glo=6;
+
+
+void sum()
+{
+    int a;
+    cout<<glo<<endl;
+}
+
+
+int main()
+{
+    int a = 4;
+    int b = 5;
+    float pi=3.141;
+    char c = 'u';
+
+    sum();
+
+    cout<<"The value of a is "<<a<<" the value of b is "<<b<<endl;
+    cout<<"The value of pi is: "<<pi<<endl;
+    cout<<"The value of char is: "<<c<<endl;
+    cout<<glo<<endl;
+
+
+    return 0;
+}
