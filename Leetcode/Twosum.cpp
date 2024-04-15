0056b2070748bfe9623e708168a455fda868db7e
Aditya <aditya.pawar23624@gmail.com>
2024-04-15 16:09:20 +0530

    LEETCODEEEEEEEEEEEEEE

---
+++ Leetcode/Twosum.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main()
+{
+    int elements;
+    cin>>elements;
+
+    vector <int> vect(elements);
+
+    for(int i=0;i<elements;i++)
+    {
+        cin>>vect.at(i);
+    }
+
+    int target;
+    cin>>target;
+
+    for(int i=0;i<elements;i++)
+    {
+        int solution = vect.at(i)+vect.at(i+1);
+
+        if(solution==target)
+        {
+            cout<<"["<<i<<","<<(i+1)<<"]"<<endl;
+        }
+    }
+
+    return 0;
+}
