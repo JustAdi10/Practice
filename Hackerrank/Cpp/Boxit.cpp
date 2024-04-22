a6cbbaaa1a365b603a2a4ae4351fd3a370e7c378
Aditya <aditya.pawar23624@gmail.com>
2024-04-22 22:58:32 +0530

    ittju

---
+++ Hackerrank/Cpp/Boxit.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+class Box
+{
+    private:
+        int l=0, b=0, h=0;
+
+    public:
+        
+}
--- Hackerrank/Cpp/StringStream.cpp
+++ Hackerrank/Cpp/StringStream.cpp
@@ -3,9 +3,27 @@
 #include <iostream>
 using namespace std;
 
-vector<int> parseInts(string str) {
-	// Complete this function
+vector <int> parseInts(string str) 
+{
+	//write code here
+    stringstream ss;
+    string temp;
+    
+    str="";
+
+     while (!ss.eof()) {
+ 
+        // Extracting word by word from stream
+        ss >> temp;
+ 
+        // Concatenating in the string to be
+        // returned
+        str = str + temp;
+    }
+    return str;
+
 }
+
 
 int main() {
     string str;
@@ -17,3 +35,25 @@ int main() {
     
     return 0;
 }
+
+
+// #include<iostream>
+// #include<sstream>
+
+// using namespace std;
+
+// int main()
+// {
+//     string s;
+//     cin>>s;
+
+//     stringstream ss(string s);
+
+//     char ch;
+//     int a,b,c;
+//     ss >> a >> ch >> b >> ch >> c;
+
+//     cout<<a<<endl;
+//     cout<<b<<endl;
+//     cout<<c<<endl;
+// }
