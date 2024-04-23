987fa43e59416cc6bfefc5776fcb82e4ba5e1b6b
Aditya <aditya.pawar23624@gmail.com>
2024-04-23 15:30:46 +0530

    flow control

--- CwH_Cpp/Tut9.cpp
+++ CwH_Cpp/Tut9.cpp
@@ -3,6 +3,41 @@ using namespace std;
 
 int main()
 {
+    //control structures
+    int age;
+    cin>>age;
 
+    if(age=18)
+    {
+        cout<<"Just legal"<<endl;
+    }
+
+    else if(age<18)
+    {
+        cout<<"non legal"<<endl;
+    }
+
+    else
+    {
+        cout<<"jfohpihsopignsOgspgSN"<<endl;
+    }
+
+
+
+    switch (age)
+    {
+    case 18:
+        /* code */
+        cout<<"You are 18"<<endl;
+        break;
+
+    case 2:
+        cout<<"You are 2"<<endl;
+        break;
+    
+    default:
+        cout<<"No special cases"<<endl;
+        break;
+    }
     return 0;
 }
