4781b4952600a43bd803f01991fe6c7344414016
Aditya <aditya.pawar23624@gmail.com>
2024-05-23 18:19:31 +0530

    Clock

--- BoomBoom/Crash.cpp
+++ BoomBoom/Crash.cpp
@@ -1,9 +1,61 @@
 #include<iostream>
 using namespace std;
+
+class time
+{
+    private:
+        int seconds,minutes,hours;
+    
+
+    public:
+    
+        int seconds_fxn()
+        {
+            while(1)
+            {
+                seconds++;
+            }
+
+            return seconds;
+        }
+
+        int minutes_fxn()
+        {
+            while(1)
+            {
+                if(seconds%60==0)
+                {
+                    minutes++;
+                }
+            }
+
+            return minutes;
+        }
+        
+        int hours_fxn()
+        {
+            while(1)
+            {
+                if(hours%60==0)
+                {
+                    hours++;
+                }
+            }
+        }
+
+        void display(int time::seconds ,int time::minutes,int time::hours)
+        {
+            while(1)
+            {
+                cout<<seconds<<" seconds "<<minutes<<" minutes "<<hours<<" hours"<<endl;
+            }
+        }
+};
+
 int main()
 {
-    while(1)
-    {
-        cout<<"卐 卍";
-    }
+    time clock;
+    clock.display;
+    
+    return 0;
 }
--- Fonny/Typetest.cpp
+++ Fonny/Typetest.cpp
@@ -1,2 +1,36 @@
 #include<iostream>
-using namespace
+using namespace std;
+
+class random
+{
+    private:
+        int a = 10;
+    
+    public:
+        int random_1 ()
+        {
+            int sum1;
+            cin>>sum1;
+
+            int sum = a + sum1;
+
+            return sum;
+        }
+};
+
+class der_random : public random
+{
+    void random_2 (int sum)
+    {
+        cout<<"The sum is "<<sum<<endl;
+    }
+};
+
+int main()
+{
+    random obj;
+    obj.random_1;
+    obj.random_2;
+
+    return 0;
+}
